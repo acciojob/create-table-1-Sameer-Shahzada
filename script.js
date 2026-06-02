0fa5b195a51a8591f0c5ba23c06f1771d7b11085
Mohd. Sameer <66036809+Sameer-Shahzada@users.noreply.github.com>
2026-06-02 16:06:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 function insert_Row() {
-
-    // Get table
     let table = document.getElementById("sampleTable");
 
-    // Insert new row at top (index 0)
     let row = table.insertRow(0);
 
-    // Insert cells
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
 
-    // Add text
     cell1.innerHTML = "New Cell1";
     cell2.innerHTML = "New Cell2";
 }
